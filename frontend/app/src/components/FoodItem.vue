5bbcfa55f6ea24de41ba70be5d2f6307498b5fbb
Dominik Sawicki <dominik.sawicki52@gmail.com>
2024-03-10 21:42:47 +0100

    Add ids

--- frontend/app/src/components/FoodItem.vue
+++ frontend/app/src/components/FoodItem.vue
@@ -7,10 +7,11 @@ defineProps({
 </script>
 
 <template>
-  <div class="bg-slate-500 w-full max-h-20 rounded p-3 mb-3">
+  <div class="bg-slate-500 w-full max-h-20 rounded px-3 py-4 mb-3 relative">
+    <div class="absolute w-[50px] text-[12px] top-0 left-0 px-1 bg-slate-600 rounded-tl">{{ food.ndb_no }}</div>
     <div class="font-bold capitalize truncate h-1/2">{{ food.descrip }}</div>
     <div class="flex justify-between">
-        <div>{{ Math.round((food.energy_kcal + Number.EPSILON) * 100) / 100 }}kcal</div>
+        <div>{{ Math.round((food.energy_kcal + Number.EPSILON) * 100) / 100 }} kcal</div>
         <div v-if="food.nutrient_score !== undefined">Nutrient score: {{ Math.round((food.nutrient_score + Number.EPSILON) * 100) / 100 }}</div>
     </div>
   </div>
